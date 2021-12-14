dc867546ede13f1e09e9ee9f00f31b163cf2da61
Fargina <farginacse@gmail.com>
2021-12-15 00:35:28 +0100

    traditional device class without SRP

---
+++ OOD/SRP/SRP/Device.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP
+{
+    /// <summary>
+    /// SRP: A class or method should have only one reason to change.
+    /// </summary>
+    public class Device
+    {
+        public int DeviceId { set; get; }
+        public string DeviceName { set; get; }
+
+        public void ReadData()
+        {
+            //
+        }
+
+        public void ProcessData()
+        {
+            //
+        }
+
+        public void SendMessageToServer()
+        {
+            //
+        }
+
+        public void ReceiveMessageFromServer()
+        {
+            //
+        }
+
+        public void LogMessage()
+        {
+            //
+        }
+
+    }
+}
